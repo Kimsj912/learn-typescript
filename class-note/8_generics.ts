851360b8a0170c9b822c6427e0325f1c938d2ec2
sujk912 <aa991122aa@gmail.com>
2022-07-17 17:50:41 +0900

    Why Generic needed

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,3 +1,4 @@
+// JS방식
 // function logText(text) {
 //     console.log(text);
 //     return text;
@@ -6,10 +7,27 @@
 // logText('hello'); // 문자열 하이
 // logText(true); // 진위값 true
 
-// generic사용법과 특징
+// Generic의 장점 1 : 함수의 중복을 막는다.
 function logText<T>(text: T) {
     console.log(text);
     return text;
 }
 logText<number>(10); // 숫자 10
 logText<string>('10'); // 문자 10
+
+function logText<T>(text: T) {
+    console.log(text);
+    return text;
+}
+logText(10); // 숫자 10
+logText('10'); // 문자 10
+logText(true); // 진위값 true
+
+
+// Generic의 장점 2 : 타입 유니온의 단점을 해결
+function logText(text: string | number) {
+    console.log(text);
+    return text;
+}
+const a = logText('a');
+a.split(''); 
