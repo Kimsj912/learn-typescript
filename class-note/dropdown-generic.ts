8b540361d69c34f456d615792c2a7630eb93798b
sujk912 <aa991122aa@gmail.com>
2022-07-17 18:12:37 +0900

    generic써서 코드 줄이기 실전예쩨

--- class-note/dropdown-generic.ts
+++ class-note/dropdown-generic.ts
@@ -1,18 +1,10 @@
 // 기본문법으로 풀면, value의 타입만 다른 대상을 위해 여러 인터페이스를 만드는 수고가 생긴다.
-interface Email {
-    value: string;
-    selected: boolean;
-}
-interface ProductNumber {
-    value: number;
-    selected: boolean;
-}
-interface TrueFalse {
-    value: boolean;
+interface Dropdown<T> { // 코드의 중복을 줄일 수 있다.
+    value: T;
     selected: boolean;
 }
 
-const emails: Email[] = [
+const emails: Dropdown<string>[] = [
     { value: 'naver.com', selected: true },
     { value: 'gmail.com', selected: false },
     { value: 'hanmail.net', selected: false },
@@ -20,13 +12,13 @@ const emails: Email[] = [
 
 
 
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: Dropdown<number>[] = [
     { value: 1, selected: true },
     { value: 2, selected: false },
     { value: 3, selected: false },
 ];
 
-function createDropdownItem(item : Email | ProductNumber) {
+function createDropdownItem(item : Dropdown<number | string>) {
     const option = document.createElement('option');
     option.value = item.value.toString();
     option.innerText = item.value.toString();
@@ -40,12 +32,3 @@ emails.forEach(function (email) {
     const selectTag = document.querySelector('#email-dropdown');
     selectTag?.appendChild(item);
 });
-
-
-// 인터페이스에서 제네릭을 사용하는 방법
-interface Dropdown<T> {
-    value: T;
-    selected: boolean;
-}
-const dbobj: Dropdown<string> = { value: 'abc', selected: false };
-
