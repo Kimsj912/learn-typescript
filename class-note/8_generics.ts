3590e6d034adbcd4b615bb49b07215f52d6953fa
sujk912 <aa991122aa@gmail.com>
2022-07-17 18:06:52 +0900

    dropdown interace with generic example

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,11 +1,13 @@
+/**
 // JS방식
-// function logText(text) {
-//     console.log(text);
-//     return text;
-// }
-// logText(10); // 숫자 10
-// logText('hello'); // 문자열 하이
-// logText(true); // 진위값 true
+function logText(text) {
+    console.log(text);
+    return text;
+}
+logText(10); // 숫자 10
+logText('hello'); // 문자열 하이
+logText(true); // 진위값 true
+
 
 // Generic의 장점 1 : 함수의 중복을 막는다.
 function logText<T>(text: T) {
@@ -31,3 +33,14 @@ function logText(text: string | number) {
 }
 const a = logText('a');
 a.split(''); 
+
+*/
+
+// type을 적어 함수를 호출하면 logText함수에선 T의 자리를 입력받은 type으로 채워넣는다. 
+function logText<T>(text: T) {
+    console.log(text);
+    return text;
+}
+// 타입추론을 통해 반환값의 타입까지도 제어가 가능하다
+let retVal: string = logText<string>('10'); 
+
---
+++ class-note/dropdown-generic.ts
@@ -0,0 +1,42 @@
+// 기본문법으로 풀면, value의 타입만 다른 대상을 위해 여러 인터페이스를 만드는 수고가 생긴다.
+interface Email {
+    value: string;
+    selected: boolean;
+}
+interface ProductNumber {
+    value: number;
+    selected: boolean;
+}
+interface TrueFalse {
+    value: boolean;
+    selected: boolean;
+}
+
+const emails: Email[] = [
+    { value: 'naver.com', selected: true },
+    { value: 'gmail.com', selected: false },
+    { value: 'hanmail.net', selected: false },
+];
+
+
+
+const numberOfProducts: ProductNumber[] = [
+    { value: 1, selected: true },
+    { value: 2, selected: false },
+    { value: 3, selected: false },
+];
+
+function createDropdownItem(item : Email | ProductNumber) {
+    const option = document.createElement('option');
+    option.value = item.value.toString();
+    option.innerText = item.value.toString();
+    option.selected = item.selected;
+    return option;
+}
+
+// NOTE: 이메일 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+    const item = createDropdownItem(email);
+    const selectTag = document.querySelector('#email-dropdown');
+    selectTag.appendChild(item);
+});
